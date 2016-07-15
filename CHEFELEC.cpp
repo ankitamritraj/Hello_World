bea34f502dc22a54af9942e31eb474ef9a9a3361
ankitamritraj <ankitamritraj07@gmail.com>
2016-07-15 12:17:45 +0530

    added chefelec

---
+++ CHEFELEC.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <map>
+#include <iterator>
+#include <stack>
+#include <queue>
+#include <utility>
+using namespace std;
+
+#define lli long long int
+#define mod 1e9+7
+#define f(i,x,n) for(i=x;i<n;i++)
+#define vlli vector<lli>
+#define pb push_back
+#define mp make_pair
+#define INF 0x3f3f3f3f
+#define INFL 0x3f3f3f3f3f3f3f3fLL
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	cout<<"Wait for the contest to end....I'll give you everything you need";
+	return 0;
+}
